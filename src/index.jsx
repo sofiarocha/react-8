9a1b040144db21f6305a7a1ef0f9e49333945bb4
Sofia <sofiaestanqueirorocha@gmail.com>
2019-05-24 14:41:49 +0100

    correct github pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
 
 import '../assets/stylesheets/application.scss';
 import App from './components/App';
@@ -8,9 +7,7 @@ import App from './components/App';
 const root = document.getElementById('root');
 if (root) {
     ReactDOM.render(
-        <BrowserRouter basename={process.env.REPO_NAME}>
-            <App />
-        </BrowserRouter>,
+        <App />,
         root
     );
 }
